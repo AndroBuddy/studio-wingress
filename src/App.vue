2d88cb87d9b2968afcc04709f0ab683efe4e8b36
Androbuddy <anirudhchimpidi8@gmail.com>
2021-05-15 10:18:42 +0530

    Setup landing page
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen p-8 text-white">
+    <Header/>
+      <div class="flex flex-1 justify-center items-center text-2xl font-bold">
+        <p class="cursor-pointer">stud/o<span class="ml-4 text-blue-400">w/ngress</span>.</p>
+      </div>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Space Mono', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #1D1A1A;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    Text
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
